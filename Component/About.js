8a59e41e46f83c1703b5a9c97c11c940380b85f8
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-10 22:53:36 +0530

    About component updated

--- Component/About.js
+++ Component/About.js
@@ -9,7 +9,23 @@ export default function About() {
                         <div className="about-heading">{Home.myActivityHeading}</div>
                     </div>
                 </div>
-                <div className="row">
+                <div className="row" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-sine">
+                    <div className="col-md-12">
+                        <div className="about-info">{Home.myActivityInfoOne}</div>
+                        <div className="about-info">{Home.myActivityInfoTwo}</div>
+                        <div className="about-info">{Home.myActivityInfoThree}</div>
+                        <div className="row" style={{ maxWidth: '300px' }}>
+                            {
+                                Home.myActivityTechInfo.map((techInfo, index) => (
+                                    <div className="col-md-6" key={index}>
+                                        <div className="about-tech">{techInfo.techName}</div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="row">
                     {
                         Home.myActivity.map((aboutInfo, index) => (
                             <div className="col-md-6" key={index}>
@@ -32,7 +48,7 @@ export default function About() {
                             </div>
                         ))
                     }
-                </div>
+                </div> */}
             </div>
         </div>
     )
