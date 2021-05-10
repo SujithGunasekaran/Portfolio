48375b4b59ad229a27b726d169d3d958e7c25278
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-10 22:54:13 +0530

    project component updated

--- Component/Project.js
+++ Component/Project.js
@@ -1,4 +1,5 @@
 import { myInfo } from '../Information/myInfo';
+import FolderOpenRoundedIcon from '@material-ui/icons/FolderOpenRounded';
 
 export default function MyProject() {
     const { Project } = myInfo;
@@ -21,16 +22,26 @@ export default function MyProject() {
                             Project.ProjectList.map((projectInfo, index) => (
                                 <div className="col-md-4" key={index}>
                                     <div className="project-card" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-sine">
-                                        <div className="project-card-title">{projectInfo.ProjectName}</div>
-                                        <div className="project-card-description">{projectInfo.ProjectDescription}</div>
-                                        <div className="project-card-tech-stack"><span className="project-card-tech-stack-language">{projectInfo.ProjectLanguage}</span></div>
-                                        <div className="row">
-                                            <div className="col-md-6">
+                                        <div className="project-card-top-display">
+                                            <FolderOpenRoundedIcon className="project-card-top-folder-icon" />
+                                            <div>
                                                 {
-                                                    projectInfo.ProjectLink !== 'No' ? <button className="project-card-website-link"><a href={projectInfo.ProjectLink} target="_blank" rel="noreference">{projectInfo.ProjectLinkBtnDisplayName}</a></button> : null
+                                                    projectInfo.ProjectGitLink !== 'No' &&
+                                                    <a href={projectInfo.ProjectGitLink} target="_blank" rel="noreference">
+                                                        <i className="fab fa-github project-card-top-font-icon"></i>
+                                                    </a>
+                                                }
+                                                {
+                                                    projectInfo.ProjectLink !== 'No' &&
+                                                    <a href={projectInfo.ProjectLink} target="_blank" rel="noreference">
+                                                        <i class="fas fa-external-link-alt project-card-top-font-icon2"></i>
+                                                    </a>
                                                 }
                                             </div>
                                         </div>
+                                        <div className="project-card-title">{projectInfo.ProjectName}</div>
+                                        <div className="project-card-description">{projectInfo.ProjectDescription}</div>
+                                        <div className="project-card-tech-stack"><span className="project-card-tech-stack-language">{projectInfo.ProjectLanguage}</span></div>
                                     </div>
                                 </div>
                             ))
